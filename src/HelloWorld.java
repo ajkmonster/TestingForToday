4826048b97a65909d9fb14b0379c7a866dfa933d
ajkmonster <46984421+ajkmonster@users.noreply.github.com>
2019-01-24 09:06:30 -0800

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+
+    // Some code here
+    // HelloWorld.main()
+
+    public static void main(String[] args) {
+
+        System.out.print("Hello Victor1");
+        System.out.println("Hello Victor2");
+        System.out.print("Hello Victor3");
+
+    }
+
+}
+
+
+
